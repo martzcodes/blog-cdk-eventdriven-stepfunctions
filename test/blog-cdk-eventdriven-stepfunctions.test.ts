6847b4c8334ce2febbe693378d935d2c2f93e62c
Matt Martz <matt.martz@gmail.com>
2022-06-24 09:51:28 -0400

    Initial commit

---
+++ test/blog-cdk-eventdriven-stepfunctions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogCdkEventdrivenStepfunctions from '../lib/blog-cdk-eventdriven-stepfunctions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-cdk-eventdriven-stepfunctions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogCdkEventdrivenStepfunctions.BlogCdkEventdrivenStepfunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
